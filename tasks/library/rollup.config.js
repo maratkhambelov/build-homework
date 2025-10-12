d13ccef0d483720b3e9f07db29cca195dc909346
MaratKhambelov <mkhambelov@ecom.tech>
2025-10-12 15:09:26 +0300

    Task 13: Сборка библиотек

--- tasks/library/rollup.config.js
+++ tasks/library/rollup.config.js
@@ -1,3 +1,19 @@
 import typescript from "@rollup/plugin-typescript";
 
-export default {};
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                preserveModules: true,
+                dir: "lib",
+                format: "es",
+            },
+        ],
+        plugins: [typescript({ tsconfig: "./tsconfig.json",
+            declaration: true,
+            declarationDir: "lib",
+            outDir: "lib",
+        })],
+    },
+];
